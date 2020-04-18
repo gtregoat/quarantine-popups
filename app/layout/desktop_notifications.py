dae4084e0764abe8b91d10a49b9373e71591b941
gtregoat <tregoat.gabriel@gmail.com>
2020-04-18 18:31:59 +0200

    Added notifications for mac

--- app/layout/desktop_notifications.py
+++ app/layout/desktop_notifications.py
@@ -1,27 +1,29 @@
 from sys import platform
+import os
 
 
 class Notifier:
     def __init__(self, *args, **kwargs):
+        """kwargs must contain msg and title"""
+        assert set(("msg", "title")).issubset(kwargs), "'msg' and 'title' must be provided to the notifier."
         if platform == "linux" or platform == "linux2":
             pass
         elif platform == "darwin":
-            # OS X
-            pass
+            self._notifier = """osascript -e 'display notification "{msg}" with title "{title}"' """
         elif platform == "win32":
             from win10toast import ToastNotifier
             self._notifier = ToastNotifier()
             self.notification_method_name = "show_toast"
-            self.args = args
-            self.kwargs = kwargs
+        # Args for all platforms
+        self.args = args
+        self.kwargs = kwargs
 
     @property
     def notify(self):
         if platform == "linux" or platform == "linux2":
             pass
         elif platform == "darwin":
-            # OS X
-            pass
+            return lambda msg, title, **kwargs: os.system(self._notifier.format(msg=msg, title=title))
         elif platform == "win32":
             return getattr(self._notifier, self.notification_method_name)
 
